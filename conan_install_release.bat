5df43b5d5000853f3384b6259b036e33e6da46b8
liuxin <liuxin@fscut.com>
2025-03-06 14:57:33 +0800

    终于执行完cmake的配置了。

---
+++ conan_install_release.bat
@@ -0,0 +1,3 @@
+conan install . --build=never -s build_type=Release --build=never -r=conancenter
+del CMakeUserPresets.json
+pause
---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class ProtoBufRecipe(ConanFile):
+    name = "protobuf"
+    settings = "os", "compiler", "build_type", "arch"
+    '这个设置必须要有，否则会出现找不到 build_type 的错误。'
+
+    generators = "CMakeDeps", "CMakeToolchain"
+    '必须设置生成器，否则 Conan 不生成 conan_toolchain.cmake 文件'
+
+    def requirements(self):
+        '依赖项'
+        self.requires("abseil/20240722.0")
+
+    def config_options(self):
+        self.options["abseil"].shared = False
+
+    def layout(self):
+        '这个函数必须要有，用于控制 conan 文件生成位置。'
+        cmake_layout(self, build_folder="build_msvc")

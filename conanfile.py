a01af1ed3b83e8c7cfaf86a1b7c36ead59b38a7c
liuxin <liuxin@fscut.com>
2025-05-13 12:00:46 +0800

    还是有问题，先提交这部分修改。

--- conanfile.py
+++ conanfile.py
@@ -8,25 +8,12 @@ class ProtoBufRecipe(ConanFile):
     version = "30.0"
     user = "third_party"
     channel = "develop"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch"
     '这个设置必须要有，否则会出现找不到 build_type 的错误。'
-
-    def requirements(self):
-        '依赖项'
-        self.requires("abseil/20250127.0@third_party/develop")
-
-    def layout(self):
-        '这个函数必须要有，用于控制 conan 文件生成位置。'
-        cmake_layout(self, build_folder="build_msvc")
     
-    def generate(self):
-        tc = CMakeToolchain(self) 
-        tc.user_presets_path = False # prevent CMakeUserPresets.json from being generated
-        for _, v in self.dependencies._data.items():
-            pkg = str(v)[:str(v).find('/')]
-            cur_dependency = self.dependencies[pkg]
-            config_path = cur_dependency.cpp_info.get_property("config_path")
-            config_name = cur_dependency.cpp_info.get_property("cmake_file_name")
-            if config_path is not None and config_name is not None:
-                tc.variables[config_name + "_DIR"] = (Path(cur_dependency.package_folder) / config_path).as_posix()
-        tc.generate()
+    def package_info(self):
+        self.cpp_info.builddirs = ["cmake"]
+        self.cpp_info.set_property("cmake_find_mode", "none")
+    
+    def package(self):
+        self.copy("*", self.source_folder, self.package_folder, keep_path=True)
